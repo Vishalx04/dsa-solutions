a22b568e06a946b40366c42756f05083ae770777
vishal <vishalx04.dev@gmail.com>
2025-11-05 17:30:37 +0530

    4sum solution added

---
+++ 4sum.cpp
@@ -0,0 +1,43 @@
+// The brute force solution is taking 4 nested loops which will have time complexity of O(n^4)
+// and space complexity of O(1)
+
+// The optimal solution is - 
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        if(n < 4) return ans;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < n - 3; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue; 
+
+            for(int j = i + 1; j < n - 2; j++) {
+                if(j > i + 1 && nums[j] == nums[j-1]) continue; 
+
+                int k = j + 1, l = n - 1;
+                long long temp = (long long)target - nums[i] - nums[j];
+
+                while(k < l) {
+                    long long sum = (long long)nums[k] + nums[l];
+                    if(sum == temp) {
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+
+                        while(k < l && nums[k] == nums[k+1]) k++;
+                        while(k < l && nums[l] == nums[l-1]) l--;
+
+                        k++;
+                        l--;
+                    }
+                    else if(sum < temp) k++;
+                    else l--;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
